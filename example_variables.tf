9d130ea47ffb23b4159eae3013430ab5276c4156
AndreHoch <39279226+ITAndreHoch@users.noreply.github.com>
2019-02-12 15:58:26 +0100

    Create example_variables.tf

---
+++ example_variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_server" {
+  description = "VSphere Server for PROD/DEV environment - Company"
+  default = "vcenter01.doamin.pl"
+}
+
+variable "vsphere_user" {
+  description = "VSphere Server user for PROD/DEV environment - Company"
+  default = "username@doamin.pl"
+}
+
+variable "vsphere_password" {
+  description = "VSphere Server password for PROD/DEV environment - Company"
+  default = "Password"
+}
